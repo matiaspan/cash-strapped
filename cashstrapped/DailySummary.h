5cd730022b59a8a226be6d893f749f95e7bee0ab
Maximiliano Gomez Vidal <mvidal@gmail.com>
2014-05-15 11:54:35 -0300

    Add daily summary entity.

---
+++ cashstrapped/DailySummary.h
@@ -0,0 +1,18 @@
+//
+//  DailySummary.h
+//  cashstrapped
+//
+//  Created by Maximiliano Gomez Vidal on 5/15/14.
+//  Copyright (c) 2014 Cannonball HQ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface DailySummary : NSManagedObject
+
+@property (nonatomic, retain) NSDate * date;
+@property (nonatomic, retain) NSNumber * amount;
+
+@end
---
+++ cashstrapped/DailySummary.m
@@ -0,0 +1,17 @@
+//
+//  DailySummary.m
+//  cashstrapped
+//
+//  Created by Maximiliano Gomez Vidal on 5/15/14.
+//  Copyright (c) 2014 Cannonball HQ. All rights reserved.
+//
+
+#import "DailySummary.h"
+
+
+@implementation DailySummary
+
+@dynamic date;
+@dynamic amount;
+
+@end
