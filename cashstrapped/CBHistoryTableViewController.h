dcccb63dca0cb5315d84ec6c05dc96f56f132653
Maximiliano Gomez Vidal <mvidal@gmail.com>
2014-05-30 15:25:22 -0300

    Create history table view controller.

---
+++ cashstrapped/CBHistoryTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  CBHistoryTableViewController.h
+//  cashstrapped
+//
+//  Created by Maximiliano Gomez Vidal on 5/30/14.
+//  Copyright (c) 2014 Cannonball HQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CBHistoryTableViewController : UITableViewController
+
+@end
